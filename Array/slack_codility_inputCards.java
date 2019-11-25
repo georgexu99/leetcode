ff7f7e522ecca4f9a19272d8d18425713f6de986
george xu <george.xu99@hotmail.com>
2019-11-24 17:53:38 -0800

    slack codility practice question

--- Array/slack_codility_inputCards.java
+++ Array/slack_codility_inputCards.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 // for each 
 
 class Solution {
-
+    // store all valid inputs in a map and give them a number value
+    // map - an unpopulated map
     public void populateMap(HashMap<String, Integer> map) {
         for(int i = 0; i<= 10; i++) {
             map.put(Integer.toString(i),i);
@@ -13,7 +14,9 @@ class Solution {
         map.put("Q",12);
         map.put("K",13);
     }
-
+    // trim the number of leading 0s and validate that it matches one of our accepted inputs
+    // s: input string to validate
+    // map: map of accepted inputs as keys, and their assigned values
     public String formatAndValidate(String s, HashMap<String, Integer> map) {
         if (map.containsKey(s.replaceFirst("^0+(?!$)", ""))) {
             return s;
@@ -23,6 +26,7 @@ class Solution {
             return "0";
         }
     }
+    // loop over the two input arrays, and return how many times jon wins
     public int JonWins(String[] Anne, String[] Jon) {
         if (Jon.length == 0) {
             return 0;
