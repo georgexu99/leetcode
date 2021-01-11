81019660e4c3782b76a983774eb23b1a1901139e
George <george.xu99@hotmail.com>
2021-01-10 18:25:03 -0800

    longest substring

---
+++ DynamicProgramming/003.longest-substring-without-repeating-characters.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let max = 0;
+    let arr = {};
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i; j < s.length; j++) {
+            if (!arr[i]) {
+                arr[i] = [];
+            }
+
+            if (!arr[i][j]) {
+                arr[i][j] = 0;
+            }
+
+            if (i === j) {
+                arr[i][j] = 1;
+            } else {
+                if (s.substring(i, j).includes(s[j])) {
+                    break;
+                } else {
+                    arr[i][j] = arr[i][j-1] + 1;
+                }
+            }
+            
+            if (arr[i][j] > max) {
+                max = arr[i][j];
+            }
+        }
+    }
+    return max;
+};
